f37d9c118d36a3820d89f7b0d635944ff18161f5
Sergiy Kulanov <sergiy_kulanov@epam.com>
2023-02-03 18:20:11 +0200

    Add some tests
    Signed-off-by: Sergiy Kulanov <sergiy_kulanov@epam.com>

--- src/main/java/com/epam/edp/demo/Hello.java
+++ src/main/java/com/epam/edp/demo/Hello.java
@@ -1,5 +1,15 @@
+package com.epam.edp.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!!!");
+    }
+
+    @Test void someLibraryMethodReturnsTrue() {
+        String env = System.getenv("TESTKUBE_GRADLE");
+        assertTrue(Boolean.parseBoolean(env), "TESTKUBE_GRADLE env should be true");
     }
 }
